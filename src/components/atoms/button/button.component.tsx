b4592887dc3364879af9f58a5b541f36dabf477a
Nathan Davies <nathan.5.davies@bt.com>
2025-08-27 11:36:53 +0100

    Light theme

--- src/components/atoms/button/button.component.tsx
+++ src/components/atoms/button/button.component.tsx
@@ -15,9 +15,10 @@ export const Button = ({
       <style jsx>
         {`
           button {
-            background: ${variant === "solid" ? "red" : "transparent"};
-            border: ${variant === "flat" ? "0" : "2px solid red"};
-            color: ${variant === "flat" ? "inherit" : "red"};
+            background: ${variant === "solid" ? "var(--button-background)" : "transparent"};
+            border: ${variant === "flat" ? "0" : "2px solid var(--button-background)"};
+            border-radius: .31em;
+            color: ${variant === "solid" ? "var(--button-color)" : "inherit"};
             cursor: pointer;
             font: inherit;
             padding: ${variant === "flat" ? "0" : ".62em 1em"};
