42a14a7fa657abc8d6706f6d8e413c9a3a4e80af
Nathan Davies <nathan.5.davies@bt.com>
2025-08-27 09:51:01 +0100

    fix: Resolve code editor syntax errors

--- src/components/organisms/code-editor/code-editor.component.tsx
+++ src/components/organisms/code-editor/code-editor.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Editor from "@monaco-editor/react";
+import Editor, { OnMount } from "@monaco-editor/react";
 import { useEffect, useState } from "react";
 
 import { getLanguageByFilename } from "./code-editor.helpers";
@@ -22,6 +22,38 @@ export const CodeEditor = ({
     return window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', handleColorSchemeChange);
   }, []);
 
+  const handleEditorDidMount = (
+    editor: Parameters<OnMount>[0],
+    monaco: Parameters<OnMount>[1],
+  ) => {
+    if (monaco && filePath?.endsWith("tsx")) {
+      monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+        allowJs: true,
+        allowNonTsExtensions: true,
+        esModuleInterop: true,
+        jsx: monaco.languages.typescript.JsxEmit.React,
+        module: monaco.languages.typescript.ModuleKind.CommonJS,
+        moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+        noEmit: true,
+        reactNamespace: "React",
+        target: monaco.languages.typescript.ScriptTarget.Latest,
+      });
+
+      monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+        noSemanticValidation: false,
+        noSyntaxValidation: false,
+      });
+    }
+
+    const model = monaco.editor.createModel(
+      code ?? "",
+      getLanguageByFilename(filePath),
+      monaco.Uri.file(filePath!)
+    );
+
+    editor.setModel(model);
+  }
+
   return (
     <>
       <section>
@@ -35,6 +67,12 @@ export const CodeEditor = ({
         </header>
         <Editor
           language={language}
+          onMount={handleEditorDidMount}
+          options={{
+            minimap: {
+              enabled: false,
+            },
+          }}
           theme={colorScheme === "light" ? "light" : "vs-dark"}
           value={code}
         />
